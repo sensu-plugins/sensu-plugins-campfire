a0a441913d283d27b291c237a3204d13cd81f49f
Matt Jones <mattjones@yieldbot.com>
2015-04-23 13:25:06 -0400

    update repo

--- scripts/gem_test.rb
+++ scripts/gem_test.rb
@@ -4,4 +4,4 @@ plugin = File.basename(File.expand_path('.'))
 
 require plugin
 
-exit 1 if ! ( SensuPluginsCampfire::Version::VER_STRING =~ ^\d*.*\d.*\d)
+exit 1 unless  SensuPluginsCampfire::Version::VER_STRING =~ /^\d*.*\d.*\d/
